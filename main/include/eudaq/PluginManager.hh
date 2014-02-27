e6bfc90fb6a005712fac8079ee9222b6ba501bf5
RPeschke <Richard.Peschke@desy.de>
2014-02-27 17:33:32 +0100

    synchronization:
    plugin manager can now access the  setCurrentTLUEvent method from the data converter

--- main/include/eudaq/PluginManager.hh
+++ main/include/eudaq/PluginManager.hh
@@ -37,7 +37,7 @@ namespace eudaq {
 	  static int IsSyncWithTLU(eudaq::Event const & ev,eudaq::TLUEvent const & tlu);
 	  static t_eventid getEventId( eudaq::Event const & ev);
 
-
+	  static void setCurrentTLUEvent(eudaq::Event & ev,eudaq::TLUEvent const & tlu);
       static void Initialize(const DetectorEvent &);
       static lcio::LCRunHeader * GetLCRunHeader(const DetectorEvent &);
       static StandardEvent ConvertToStandard(const DetectorEvent &);
