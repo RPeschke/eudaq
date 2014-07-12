2793d13e3cf741c4dcdddbdca113862caa7550d7
RPeschke <Richard.Peschke@desy.de>
2014-07-12 10:25:44 +0200

    meta data are no longer a nested class. I expect we need this class outside of the aida packet.

--- main/include/eudaq/AidaPacket.hh
+++ main/include/eudaq/AidaPacket.hh
@@ -16,6 +16,7 @@
 #include "eudaq/Utils.hh"
 #include "eudaq/SmartEnum.hh"
 #include "eudaq/Platform.hh"
+#include "eudaq/aidaMetaData.hh"
 
 #define EUDAQ_DECLARE_PACKET()                  \
   public:                                       \
@@ -47,18 +48,7 @@ namespace eudaq {
 #define PACKET_TYPE_BITS    4
 #define PACKET_TYPE_SHIFT 	60
 
-// meta data
-#define TLU_BITS   1
-#define TLU_SHIFT 63
 
-#define ENTRY_TYPE_BITS   4
-#define ENTRY_TYPE_SHIFT 59
-
-#define COUNTER_BITS  59
-#define COUNTER_SHIFT  0
-
-#define getBits(FIELD,from)	getBitsTemplate<FIELD ## _SHIFT, FIELD ## _BITS>(from)
-#define setBits(FIELD,dst, val)	setBitsTemplate<FIELD ## _SHIFT, FIELD ## _BITS>(dst,val)
 
 
 class DLLEXPORT AidaPacket : public Serializable {
@@ -110,35 +100,13 @@ class DLLEXPORT AidaPacket : public Serializable {
 
 	DECLARE_ENUM_CLASS( MetaDataType, RUN_NUMBER, TRIGGER_COUNTER, TRIGGER_TIMESTAMP );
 
-    class MetaData {
-      public:
-    	std::vector<uint64_t> v;
 
-    	void add( bool tlu, int type, uint64_t data ) {
-        		uint64_t meta_data = 0;
-        		SetType( meta_data, type );
-        		SetCounter( meta_data, data );
-        		v.push_back( meta_data );
-    	};
-    };
 
     MetaData& GetMetaData() {
     	return m_meta_data;
     }
 
-    static inline int GetType( uint64_t meta_data ) { return getBits(ENTRY_TYPE, meta_data ); };
-    static inline void SetType( uint64_t& meta_data, int type ) { setBits(ENTRY_TYPE,  meta_data, type ); };
-    static inline bool IsTLUBitSet( uint64_t meta_data ) { return getBits(TLU, meta_data ); };
-    static inline uint64_t GetCounter( uint64_t meta_data ) { return getBits(COUNTER, meta_data ); };
-    static inline void SetCounter( uint64_t& meta_data, uint64_t data ) { setBits(COUNTER, meta_data, data ); };
 
-    template <int shift, int bits> static uint64_t getBitsTemplate( uint64_t from ) {
-    	return shift > 0 ? (from >> shift) & bit_mask()[bits] : from & bit_mask()[bits];
-    };
-
-    template <int shift, int bits> static void setBitsTemplate( uint64_t& dest, uint64_t val ) {
-    	dest |= shift > 0 ? (val & bit_mask()[bits]) << shift : val & bit_mask()[bits];
-    };
 
 
     void SetData( uint64_t* data, uint64_t size ) {
@@ -175,7 +143,7 @@ class DLLEXPORT AidaPacket : public Serializable {
     void SerializeMetaData( Serializer & ) const;
 
     static PacketHeader DeserializeHeader( Deserializer & );
-    static const uint64_t * const bit_mask();
+   
 
     PacketHeader m_header;
     MetaData m_meta_data;
