9cb04ee5c53c6b06fa0e3bc73b18f39c9288b2a2
Richard Peschke <Richard.Peschke@desy.de>
2014-08-02 10:57:59 +0200

    implemented functionality to generic detector container

--- main/include/eudaq/genericDetectorContainer.hh
+++ main/include/eudaq/genericDetectorContainer.hh
@@ -9,23 +9,28 @@
 
 template <typename T>
 class genericDetContainer{
-  typedef T::t_id ELement_id;
+public:
+  typedef typename T::t_id ELement_id;
   typedef std::shared_ptr<T> epointer; // element Pointer 
 
-  genericDetContainer(size_t expected_Number_of_Elemtns = NOEXPECTATION) : m_container(expected_Number_of_Elemtns), (expected_Number_of_Elemtns){}
-  void add_element(epointer newElement){m_container.push_back(newElement)}
+  genericDetContainer(size_t expected_Number_of_Elemtns = NOEXPECTATION) : 
+    m_container(expected_Number_of_Elemtns), 
+    m_expected_elements(expected_Number_of_Elemtns)
+  {  }
+
+
+  void add_element(epointer newElement){ m_container.push_back(newElement); }
   epointer getElement(size_t index){ return m_container.at(index); }
   epointer getElement(ELement_id id){
-    auto ret=std::find(m_container.begin(), m_container.end(),
-      [&id](epointer e){
-        return e->getID() == id;
-      }
-    )
-    if (ret!=m_container.end())
+    for (auto e:m_container)
     {
-        return *ret;
+      if (e->getID()==id)
+      {
+        return e;
+      }
     }
-      return nullptr;
+  
+    return nullptr;
   }
 
 
