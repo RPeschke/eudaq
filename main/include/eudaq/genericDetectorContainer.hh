eaf7283735ec85b3dbc0c24a38ccb29cca97c275
Richard Peschke <Richard.Peschke@desy.de>
2014-08-02 11:01:59 +0200

    generic detector container is now part of EUDAQ name space

--- main/include/eudaq/genericDetectorContainer.hh
+++ main/include/eudaq/genericDetectorContainer.hh
@@ -6,40 +6,41 @@
 
 
 #define NOEXPECTATION 0
+namespace eudaq{
 
-template <typename T>
-class genericDetContainer{
-public:
-  typedef typename T::t_id ELement_id;
-  typedef std::shared_ptr<T> epointer; // element Pointer 
+  template <typename T>
+  class genericDetContainer{
+  public:
+    typedef typename T::t_id ELement_id;
+    typedef std::shared_ptr<T> epointer; // element Pointer 
 
-  genericDetContainer(size_t expected_Number_of_Elemtns = NOEXPECTATION) : 
-    m_container(expected_Number_of_Elemtns), 
-    m_expected_elements(expected_Number_of_Elemtns)
-  {  }
+    genericDetContainer(size_t expected_Number_of_Elemtns = NOEXPECTATION) :
+      m_container(expected_Number_of_Elemtns),
+      m_expected_elements(expected_Number_of_Elemtns)
+    {  }
 
 
-  void add_element(epointer newElement){ m_container.push_back(newElement); }
-  epointer getElement(size_t index){ return m_container.at(index); }
-  epointer getElement(ELement_id id){
-    for (auto e:m_container)
-    {
-      if (e->getID()==id)
+    void add_element(epointer newElement){ m_container.push_back(newElement); }
+    epointer getElement(size_t index){ return m_container.at(index); }
+    epointer getElement(ELement_id id){
+      for (auto e : m_container)
       {
-        return e;
+        if (e->getID() == id)
+        {
+          return e;
+        }
       }
+
+      return nullptr;
     }
-  
-    return nullptr;
-  }
 
 
 
-private:
+  private:
 
-  std::vector<epointer> m_container;
-  size_t m_expected_elements;
-};
-
+    std::vector<epointer> m_container;
+    size_t m_expected_elements;
+  };
+}
 
 #endif // genericDetectorContainer_h__
