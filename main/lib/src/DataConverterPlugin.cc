637149f591ade1dd460b25b80535273ca427ce79
Richard Peschke <Richard.Peschke@desy.de>
2014-08-05 16:33:32 +0200

    the "data converter plugin" is now Templated. to prevent cyclic dependenses between data converter and plugin manager some of the functions are put to the source files.
    to instantiate them they need to have somebody who calls them. to make this automatically there is this precompiler macro which creates a function for this

--- main/lib/src/DataConverterPlugin.cc
+++ main/lib/src/DataConverterPlugin.cc
@@ -1,26 +1,37 @@
 #include "eudaq/DataConverterPlugin.hh"
 #include "eudaq/PluginManager.hh"
-
 #include <iostream>
+#define RegisterNewDataConverterType(type) void temp1(eudaq::DataConverterPlugin<type>::mainType &mT, eudaq::DataConverterPlugin<type>::subType &sT){\
+                                                TemporaryFunction1<type>(mT, sT);\
+                                            } \
+                                            const int dummy1=0
+
 
 namespace eudaq {
 
-  unsigned DataConverterPlugin::GetTriggerID(eudaq::Event const &) const {
-    return (unsigned)-1;
-  }
-
-  DataConverterPlugin::DataConverterPlugin(std::string subtype)
+  template <typename containerT>
+  DataConverterPlugin<containerT>::DataConverterPlugin(subType subtype)
     : m_eventtype(make_pair(Event::str2id("_RAW"), subtype))
   {
     //std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
-    PluginManager::GetInstance().RegisterPlugin(this);
+    PluginManager<containerT>::GetInstance().RegisterPlugin(this);
   }
-
-  DataConverterPlugin::DataConverterPlugin(unsigned type, std::string subtype)
+  template <typename containerT>
+  DataConverterPlugin<containerT>::DataConverterPlugin(mainType type, subType subtype)
     : m_eventtype(make_pair(type, subtype))
   {
     //std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
-    PluginManager::GetInstance().RegisterPlugin(this);
+    PluginManager<containerT>::GetInstance().RegisterPlugin(this);
   }
 
+
+
 }//namespace eudaq
+
+
+template <typename T>
+void TemporaryFunction1(typename eudaq::DataConverterPlugin<T>::mainType &mT, typename eudaq::DataConverterPlugin<T>::subType &sT)
+{
+  eudaq::DataConverterPlugin<T> a(sT);
+  eudaq::DataConverterPlugin<T> b(mT, sT);
+}
