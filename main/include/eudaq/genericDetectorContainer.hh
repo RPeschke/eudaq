debf14cca318375b339fa9e289b0c81101b169df
Richard Peschke <Richard.Peschke@desy.de>
2014-08-05 16:37:02 +0200

    improved functionality to create this object from one of its conainerT objects

--- main/include/eudaq/genericDetectorContainer.hh
+++ main/include/eudaq/genericDetectorContainer.hh
@@ -14,11 +14,22 @@ namespace eudaq{
     typedef typename T::t_id ELement_id;
     typedef std::shared_ptr<T> epointer; // element Pointer 
 
+    genericDetContainer(T& t){
+      
+    }
     genericDetContainer(size_t expected_Number_of_Elemtns = NOEXPECTATION) :
       m_container(expected_Number_of_Elemtns),
       m_expected_elements(expected_Number_of_Elemtns)
-    {  }
-
+    {
+     
+    }
+    genericDetContainer(unsigned runnumber, unsigned eventnumber, uint64_t timestamp):
+      m_runnumber(runnumber),
+      m_eventnumber(eventnumber),
+      m_timestamp(timestamp)
+    {
+      
+    }
 
     void add_element(epointer newElement){ m_container.push_back(newElement); }
     epointer getElement(size_t index){ return m_container.at(index); }
@@ -34,12 +45,18 @@ namespace eudaq{
       return nullptr;
     }
 
-
+    size_t Size(){
+      return m_container.size();
+    }
 
   private:
 
     std::vector<epointer> m_container;
     size_t m_expected_elements;
+    unsigned m_runnumber = 0;
+    unsigned m_eventnumber = 0;
+    uint64_t m_timestamp = 0;
+
   };
 }
 
