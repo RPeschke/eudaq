bbf6b6610820e817c2b92a5208e53edd0fa84aa8
Richard Peschke <Richard.Peschke@desy.de>
2014-10-21 10:53:03 +0200

    adding default value

--- main/include/eudaq/eventsOfIntrest.hh
+++ main/include/eudaq/eventsOfIntrest.hh
@@ -9,13 +9,15 @@ public:
   typedef std::pair<containerT,size_t> elementOfIntresst;
    
 
+  eventsOfIntresst(containerT Container, size_t Element=0) :m_container(Container, Element){
 
+  }
   void addElement(elementOfIntresst ev){
     if (!IsElement(ev)){
       m_container.push_back(ev);
     }
  }
-  void addElement(containerT Container, size_t Element){
+  void addElement(containerT Container, size_t Element=0){
     addElement(elementOfIntresst(Container, Element));
   }
 
