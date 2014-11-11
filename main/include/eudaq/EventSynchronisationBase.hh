1b0b73a158f90b5ab5be5fcec4a8922405ebf39d
RPeschke <Richard.Peschke@desy.de>
2014-11-11 23:48:32 +0100

    formating

--- main/include/eudaq/EventSynchronisationBase.hh
+++ main/include/eudaq/EventSynchronisationBase.hh
@@ -13,65 +13,65 @@
 // The Idea is that the user can define a condition that need to be true to define if an event is sync or not
 
 namespace eudaq{
-  
+
 
 
 
   class DLLEXPORT SyncBase {
-    public:
-     	 typedef std::queue<std::shared_ptr<eudaq::Event>> eventqueue_t ;
+  public:
+    typedef std::queue<std::shared_ptr<eudaq::Event>> eventqueue_t;
 
 
-	 int AddDetectorEventToProducerQueue(int fileIndex,std::shared_ptr<eudaq::DetectorEvent> dev );
-	 int AddEventToProducerQueue(int fileIndex, std::shared_ptr<eudaq::Event> Ev);
-	 virtual bool SyncFirstEvent();
-	 virtual bool SyncNEvents(size_t N);
-	 virtual bool getNextEvent( std::shared_ptr<eudaq::DetectorEvent>  & ev);
+    int AddDetectorEventToProducerQueue(int fileIndex, std::shared_ptr<eudaq::DetectorEvent> dev);
+    int AddEventToProducerQueue(int fileIndex, std::shared_ptr<eudaq::Event> Ev);
+    virtual bool SyncFirstEvent();
+    virtual bool SyncNEvents(size_t N);
+    virtual bool getNextEvent(std::shared_ptr<eudaq::DetectorEvent>  & ev);
 
-	 virtual bool compareTLUwithEventQueue(std::shared_ptr<eudaq::Event>& tlu_event,SyncBase::eventqueue_t& event_queue);
-	 virtual bool compareTLUwithEventQueues(std::shared_ptr<eudaq::Event>& tlu_event);
+    virtual bool compareTLUwithEventQueue(std::shared_ptr<eudaq::Event>& tlu_event, SyncBase::eventqueue_t& event_queue);
+    virtual bool compareTLUwithEventQueues(std::shared_ptr<eudaq::Event>& tlu_event);
 
-	 void storeCurrentOrder();
-	 bool Event_Queue_Is_Empty();
-	 bool SubEventQueueIsEmpty(int i);
-	 void event_queue_pop();
-	 void event_queue_pop_TLU_event();
-	 void makeDetectorEvent();
-	 void clearDetectorQueue();
+    void storeCurrentOrder();
+    bool Event_Queue_Is_Empty();
+    bool SubEventQueueIsEmpty(int i);
+    void event_queue_pop();
+    void event_queue_pop_TLU_event();
+    void makeDetectorEvent();
+    void clearDetectorQueue();
 
-      /** The empty destructor. Need to add it to make it virtual.
-       */
-	 virtual ~SyncBase() {}
-	 SyncBase(bool sync=true);
-	 void addBOREDetectorEvent(int fileIndex,const eudaq::DetectorEvent& BOREvent);
-	 void addBORE_Event(int fileIndex, const eudaq::Event& BOREEvent);
-	 void PrepareForEvents();
+    /** The empty destructor. Need to add it to make it virtual.
+     */
+    virtual ~SyncBase() {}
+    SyncBase(bool sync = true);
+    void addBOREDetectorEvent(int fileIndex, const eudaq::DetectorEvent& BOREvent);
+    void addBORE_Event(int fileIndex, const eudaq::Event& BOREEvent);
+    void PrepareForEvents();
 
-    protected:
+  protected:
 
-		eventqueue_t& getQueuefromId(unsigned producerID);
-		eventqueue_t& getQueuefromId(unsigned fileIndex,unsigned eventIndex);
-	
-		eventqueue_t& getFirstTLUQueue();
-		unsigned getUniqueID(unsigned fileIndex,unsigned eventIndex);
-		unsigned getTLU_UniqueID(unsigned fileIndex);
-		std::map<unsigned,size_t> m_ProducerId2Eventqueue;
-		size_t m_registertProducer;
-		std::vector<size_t> m_EventsProFileReader;
-		/* This vector saves for each producer an event queue */
-		
-		std::vector<eventqueue_t> m_ProducerEventQueue;
-		
-		std::queue<std::shared_ptr<eudaq::DetectorEvent>> m_DetectorEventQueue;
-	
+    eventqueue_t& getQueuefromId(unsigned producerID);
+    eventqueue_t& getQueuefromId(unsigned fileIndex, unsigned eventIndex);
 
-		int m_TLUs_found;
+    eventqueue_t& getFirstTLUQueue();
+    unsigned getUniqueID(unsigned fileIndex, unsigned eventIndex);
+    unsigned getTLU_UniqueID(unsigned fileIndex);
+    std::map<unsigned, size_t> m_ProducerId2Eventqueue;
+    size_t m_registertProducer;
+    std::vector<size_t> m_EventsProFileReader;
+    /* This vector saves for each producer an event queue */
 
-		bool isAsync_;
-		size_t NumberOfEventsToSync_;
-		uint64_t longTimeDiff_;
-		
-		bool m_sync;
+    std::vector<eventqueue_t> m_ProducerEventQueue;
+
+    std::queue<std::shared_ptr<eudaq::DetectorEvent>> m_DetectorEventQueue;
+
+
+    int m_TLUs_found;
+
+    bool isAsync_;
+    size_t NumberOfEventsToSync_;
+    uint64_t longTimeDiff_;
+
+    bool m_sync;
   };
 
 }//namespace eudaq
