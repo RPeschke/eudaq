944a0dd6b08b7444c7a7608a655b381423985700
RPeschke <Richard.Peschke@desy.de>
2014-12-08 17:58:37 +0100

    registered this base class to the Factory.
    removed all unneeded functions.
    
    left the old factory class in place so that the old code doesn't get broken.

--- main/include/eudaq/FileWriter.hh
+++ main/include/eudaq/FileWriter.hh
@@ -1,66 +1,57 @@
 #ifndef EUDAQ_INCLUDED_FileWriter
 #define EUDAQ_INCLUDED_FileWriter
 
-#include "eudaq/DetectorEvent.hh"
 #include <vector>
 #include <string>
-#define TAGNAME_OUTPUTPATTER "outpattern"
 
+#include "eudaq/DetectorEvent.hh"
+#include "eudaq/factory.hh"
+
+#define TAGNAME_OUTPUTPATTER "outpattern"
+#define registerFileWriter(DerivedFileWriter,ID)  registerClass(FileWriter,DerivedFileWriter,ID)
 
 namespace eudaq {
 
   class DLLEXPORT FileWriter {
-    public:
-      FileWriter();
-      virtual void StartRun(unsigned runnumber) = 0;
-      virtual void WriteBaseEvent(const Event&);
-      virtual void WriteEvent(const DetectorEvent &) = 0;
-      virtual uint64_t FileBytes() const = 0;
-      void SetFilePattern(const std::string & p) { m_filepattern = p; }
-      virtual ~FileWriter() {}
-    protected:
-      std::string m_filepattern;
+  public:
+    using MainType = std::string;
+    using Parameter_t = std::string;
+    using Parameter_ref = const Parameter_t&;
+    FileWriter();
+    virtual void StartRun(unsigned runnumber) = 0;
+    virtual void WriteBaseEvent(const Event&);
+    virtual void WriteEvent(const DetectorEvent &) = 0;
+    virtual uint64_t FileBytes() const = 0;
+    void SetFilePattern(const std::string & p) { m_filepattern = p; }
+    virtual ~FileWriter() {}
+  protected:
+    std::string m_filepattern;
   };
 
- 
-  inline void helper_setParameter(FileWriter& writer,const std::string& tagName,const std::string& tagValue){
-	if (tagName.compare(TAGNAME_OUTPUTPATTER)==0)
-	{
-		//std::cout<<"set output pattern to: "<<tagValue<<std::endl;
-		writer.SetFilePattern(tagValue);
-	}
-	  
+
+  inline void helper_setParameter(FileWriter& writer, const std::string& tagName, const std::string& tagValue){
+    if (tagName.compare(TAGNAME_OUTPUTPATTER) == 0)
+    {
+      writer.SetFilePattern(tagValue);
+    }
+
   }
   inline void helper_ProcessEvent(FileWriter& writer, const Event &ev){ writer.WriteBaseEvent(ev); }
-  inline void helper_StartRun(FileWriter& writer,unsigned runnumber){writer.StartRun(runnumber);}
+  inline void helper_StartRun(FileWriter& writer, unsigned runnumber){ writer.StartRun(runnumber); }
   inline void helper_EndRun(FileWriter& writer){};
 
 
+
+
+
   class DLLEXPORT FileWriterFactory {
-    public:
-      static FileWriter * Create(const std::string & name, const std::string & params = "");
-      template <typename T>
-        static void Register(const std::string & name) {
-          do_register(name, filewriterfactory<T>);
-        }
-      typedef FileWriter * (*factoryfunc)(const std::string &);
-      static std::vector<std::string> GetTypes();
-    private:
-      template <typename T>
-        static FileWriter * filewriterfactory(const std::string & params) {
-          return new T(params);
-        }
-      static void do_register(const std::string & name, factoryfunc);
+  public:
+    static std::unique_ptr<FileWriter> Create(const std::string & name, const std::string & params = "");
+
+    static std::vector<std::string> GetTypes();
   };
-
-  template <typename T>
-    class RegisterFileWriter {
-      public:
-        RegisterFileWriter(const std::string & name) {
-          FileWriterFactory::Register<T>(name);
-        }
-    };
-
 }
+
+
 
 #endif // EUDAQ_INCLUDED_FileWriter
--- main/lib/src/FileWriter.cc
+++ main/lib/src/FileWriter.cc
@@ -1,37 +1,12 @@
 #include "eudaq/FileWriter.hh"
 #include "eudaq/FileNamer.hh"
 #include "eudaq/Exception.hh"
+#include "eudaq/factoryDev.hh"
+
 
 namespace eudaq {
 
-  namespace {
-    typedef std::map<std::string, FileWriterFactory::factoryfunc> map_t;
-
-    static map_t & FileWriterMap() {
-      static map_t m;
-      return m;
-    }
-  }
-
-  void FileWriterFactory::do_register(const std::string & name, FileWriterFactory::factoryfunc func) {
-    //std::cout << "DEBUG: Registering FileWriter: " << name << std::endl;
-    FileWriterMap()[name] = func;
-  }
-
-  FileWriter * FileWriterFactory::Create(const std::string & name, const std::string & params) {
-    map_t::const_iterator it = FileWriterMap().find(name == "" ? "native" : name);
-    if (it == FileWriterMap().end()) EUDAQ_THROW("Unknown file writer: " + name);
-    return (it->second)(params);
-  }
-
-  std::vector<std::string> FileWriterFactory::GetTypes() {
-    std::vector<std::string> result;
-    for (map_t::const_iterator it = FileWriterMap().begin(); it != FileWriterMap().end(); ++it) {
-      result.push_back(it->first);
-    }
-    return result;
-  }
-
+  
   FileWriter::FileWriter() : m_filepattern(FileNamer::default_pattern) {}
 
   void FileWriter::WriteBaseEvent(const Event& ev)
@@ -46,4 +21,19 @@ namespace eudaq {
     }
   }
 
+
+
+  std::unique_ptr<FileWriter> FileWriterFactory::Create(const std::string & name, const std::string & params /*= ""*/)
+  {
+    return EUDAQ_Utilities::Factory<FileWriter>::Create(name, params);
+  }
+
+  std::vector<std::string> FileWriterFactory::GetTypes()
+  {
+    return EUDAQ_Utilities::Factory<FileWriter>::GetTypes();
+  }
+
+  registerBaseClassDev(FileWriter);
+
+ 
 }
