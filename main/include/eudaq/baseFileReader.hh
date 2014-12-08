72e89e9523debe95bdf12585924285df0a08484b
RPeschke <Richard.Peschke@desy.de>
2014-12-08 17:38:50 +0100

    moved implementation to src file.
    added new class which uses the Factory template class.
    
    registered base class at the Factory template

--- main/include/eudaq/baseFileReader.hh
+++ main/include/eudaq/baseFileReader.hh
@@ -3,26 +3,32 @@
 #include <string>
 #include <memory>
 #include "Platform.hh"
+#include "eudaq/factory.hh"
+#define RegisterFileReader(derivedClass,ID) registerClass(baseFileReader,derivedClass,ID)
 
 
 namespace eudaq{
-	class Event;
-	class DLLEXPORT baseFileReader{
-	public:
-		baseFileReader(std::string fileName) :m_fileName(std::move(fileName)){}
-		std::string Filename()const  { return m_fileName; };
-		virtual unsigned RunNumber() const = 0;
-		virtual std::shared_ptr<eudaq::Event> GetNextEvent()=0;
-		virtual void Interrupt(){}
-		
-	private:
-		std::string m_fileName;
+  class Event;
+  class DLLEXPORT baseFileReader{
+  public:
+    using MainType = std::string;
+    using Parameter_t = std::string;
+    using Parameter_ref = std::vector<Parameter_t>;
+    baseFileReader(Parameter_ref fileName);
+    baseFileReader(const std::string&  fileName);
+    std::string Filename()const;
+    virtual unsigned RunNumber() const = 0;
+    virtual std::shared_ptr<eudaq::Event> GetNextEvent() = 0;
+    virtual void Interrupt();
 
-	};
+  private:
+    std::string m_fileName;
+
+  };
 
 
 
-	std::shared_ptr<baseFileReader> Factory_file_reader(const std::string & filename, const std::string & filepattern );
+  std::unique_ptr<baseFileReader> DLLEXPORT Factory_file_reader(const std::string & filename, const std::string & filepattern="");
 }
 
 #endif // baseFileReader_h__
---
+++ main/lib/src/baseFileReader.cc
@@ -0,0 +1,81 @@
+#include <list>
+#include "eudaq/baseFileReader.hh"
+#include "eudaq/factoryDev.hh"
+#include "eudaq/Exception.hh"
+
+namespace eudaq {
+
+
+
+
+
+  baseFileReader::baseFileReader(Parameter_ref fileName) :baseFileReader(fileName[0])
+  {
+
+  }
+
+  baseFileReader::baseFileReader(const std::string& fileName) : m_fileName(fileName)
+  {
+
+  }
+
+  std::string baseFileReader::Filename() const
+  {
+    return m_fileName;
+  }
+
+  void baseFileReader::Interrupt()
+  {
+
+  }
+
+  registerBaseClassDev(baseFileReader);
+
+  std::unique_ptr<baseFileReader> Factory_file_reader(const std::string & filename, const std::string & filepattern)
+  {
+    // return nullptr;
+    baseFileReader::Parameter_ref m;
+    m.push_back(filename);
+    m.push_back(filepattern);
+    if (filename.find_first_not_of("0123456789") == std::string::npos) {
+      // filename is run number. using default file reader
+      return EUDAQ_Utilities::Factory<baseFileReader>::Create("raw", m);
+
+    }
+    else
+    {
+      auto index_raute =filename.find_last_of('#');
+      if (index_raute==std::string::npos)
+      {
+        // no explicit type definition 
+        auto index_dot = filename.find_last_of('.');
+        if (index_dot!=std::string::npos)
+        {
+          std::string type = filename.substr(index_dot + 1);
+          return EUDAQ_Utilities::Factory<baseFileReader>::Create(type, m);
+
+
+        }
+        else{
+          EUDAQ_THROW("unknown file type ");
+        }
+
+
+      }
+      else
+      {
+        // Explicit definition
+        std::string type = filename.substr(index_raute + 1);
+        return EUDAQ_Utilities::Factory<baseFileReader>::Create(type, m);
+
+      }
+
+
+    }
+
+
+    return nullptr;
+   
+  }
+
+}
