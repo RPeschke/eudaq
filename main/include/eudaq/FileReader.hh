d467a0ed5f99d90f988cc12dac43c6f4df211978
RPeschke <Richard.Peschke@desy.de>
2014-12-15 23:06:14 +0100

    formating

--- main/include/eudaq/FileReader.hh
+++ main/include/eudaq/FileReader.hh
@@ -13,36 +13,36 @@
 namespace eudaq {
 
 
-  class DLLEXPORT FileReader: public baseFileReader {
-    public:
-      FileReader(const std::string & filename, const std::string & filepattern = "");
+  class DLLEXPORT FileReader : public baseFileReader {
+  public:
+    FileReader(const std::string & filename, const std::string & filepattern = "");
 
-      FileReader(Parameter_ref param);
-
-
-      ~FileReader();
-      bool NextEvent(size_t skip = 0);
+    FileReader(Parameter_ref param);
 
 
+    ~FileReader();
     virtual  unsigned RunNumber() const;
+    virtual bool NextEvent(size_t skip = 0);
 
-      const eudaq::Event & GetEvent() const;
-      const DetectorEvent & Event() const { return GetDetectorEvent(); } // for backward compatibility
-	  virtual std::shared_ptr<eudaq::Event> GetNextEvent();
-	  const DetectorEvent & GetDetectorEvent() const;
-      const StandardEvent & GetStandardEvent() const;
-	  std::shared_ptr<eudaq::DetectorEvent> GetDetectorEvent_ptr(){return std::dynamic_pointer_cast<eudaq::DetectorEvent>(m_ev);};
-      virtual void Interrupt() { m_des.Interrupt(); }
-      
-    private:
-      
-      FileDeserializer m_des;
-     std::shared_ptr<eudaq::Event> m_ev;
-      unsigned m_ver;
-      size_t m_subevent_counter = 0;
+
+
+    const eudaq::Event & GetEvent() const;
+    const DetectorEvent & Event() const { return GetDetectorEvent(); } // for backward compatibility
+    virtual std::shared_ptr<eudaq::Event> GetNextEvent();
+    const DetectorEvent & GetDetectorEvent() const;
+    const StandardEvent & GetStandardEvent() const;
+    std::shared_ptr<eudaq::DetectorEvent> GetDetectorEvent_ptr(){ return std::dynamic_pointer_cast<eudaq::DetectorEvent>(m_ev); };
+    virtual void Interrupt() { m_des.Interrupt(); }
+
+  private:
+
+    FileDeserializer m_des;
+    std::shared_ptr<eudaq::Event> m_ev;
+    unsigned m_ver;
+    size_t m_subevent_counter = 0;
 
   };
- 
+
 }
 
 #endif // EUDAQ_INCLUDED_FileReader
