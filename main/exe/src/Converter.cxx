a35a2762b8e268d0daacf8ce79369ea8355f69de
RPeschke <Richard.Peschke@desy.de>
2015-01-26 15:02:06 +0100

    first Program which adapts the changes to the interfaces

--- main/exe/src/Converter.cxx
+++ main/exe/src/Converter.cxx
@@ -21,45 +21,27 @@ int main(int, char ** argv) {
   auto events = ReadAndProcess<eudaq::FileWriter>::add_Command_line_option_EventsOfInterest(op);
 
 
-  auto type = FileWriterFactory::add_Command_line_option_OutputTypes(op);
-  auto opat = FileWriterFactory::add_Command_line_option_OutputPattern(op);
+  FileWriterFactory::addComandLineOptions(op);
   op.ExtraHelpText(FileWriterFactory::Help_text());
 
-  auto ipat = multiFileReader::add_Command_line_option_inputPattern(op);
-  op.ExtraHelpText(multiFileReader::help_text());
+  
+  FileReaderFactory::addComandLineOptions(op);
 
+  op.ExtraHelpText(FileReaderFactory::Help_text());
 
-  eudaq::OptionFlag async(op, "a", "nosync", "Disables Synchronization with TLU events");
-  eudaq::Option<std::string> level(op, "l", "log-level", "INFO", "level",
+  EventSyncFactory::addComandLineOptions(op);
+
+    eudaq::Option<std::string> level(op, "l", "log-level", "INFO", "level",
                                    "The minimum level for displaying log messages locally");
   try {
     op.Parse(argv);
     EUDAQ_LOG_LEVEL(level.Value());
 
-    ReadAndProcess<eudaq::FileWriter> readProcess(!async.Value());
+    ReadAndProcess<eudaq::FileWriter> readProcess;
     readProcess.setEventsOfInterest(parsenumbers(events->Value()));
     
-    if (op.NumArgs() == 1)
-    {
-      readProcess.addFileReader(eudaq::Factory_file_reader(op.GetArg(0), ipat->Value()));
-
-    }
-    else
-    {
-      auto multiReader = std::unique_ptr<baseFileReader>(new multiFileReader());
-      auto mReader = dynamic_cast<eudaq::multiFileReader*>(multiReader.get());
-      mReader->addSyncAlgorithm(std::unique_ptr<SyncBase>(new syncToDetectorEvents(true)));
-      for (size_t i = 0; i < op.NumArgs(); ++i) {
-       mReader->addFileReader(eudaq::Factory_file_reader(op.GetArg(i), ipat->Value()));
-
-      }
-
-      readProcess.addFileReader(std::move(multiReader));
-     
-    }
-    readProcess.setWriter(FileWriterFactory::Create(type->Value()));
-
-    readProcess.SetParameter(TAGNAME_OUTPUTPATTER, opat->Value());
+    readProcess.addFileReader(FileReaderFactory::create(op));
+    readProcess.setWriter(FileWriterFactory::Create());
 
     readProcess.StartRun();
     readProcess.process();
