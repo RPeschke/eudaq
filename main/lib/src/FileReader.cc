fe9dd7a75d04a1edfede0f94990a956cbf32fdac
RPeschke <Richard.Peschke@desy.de>
2015-01-26 14:59:08 +0100

    removed magic numbers

--- main/lib/src/FileReader.cc
+++ main/lib/src/FileReader.cc
@@ -23,7 +23,7 @@ namespace eudaq {
 
   }
 
-  FileReader::FileReader(Parameter_ref param) :FileReader(param[0], param[1])
+  FileReader::FileReader(Parameter_ref param) :FileReader(param[File_name], param[Input_pattern])
   {
 
   }
