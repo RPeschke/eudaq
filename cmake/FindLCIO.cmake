b58431c9265ce6d63e1fcc5eebfd3ccb6c5967ee
Richard Peschke <Richard.Peschke@desy.de>
2015-04-08 19:27:23 +0200

    new flag to use LCIO
    find LCIO should find the release and the debug version of the file

--- cmake/FindLCIO.cmake
+++ cmake/FindLCIO.cmake
@@ -4,14 +4,31 @@
 #  LCIO_INCLUDE_DIRS - The LCIO include directories
 #  LCIO_LIBRARIES - The libraries needed to use LCIO
 #  LCIO_DEFINITIONS - Compiler switches required for using LCIO
+IF(WIN32)
+  find_path(LCIO_INCLUDE_DIR lcio.h
+    HINTS "${LCIO}/include" "$ENV{LCIO}/include" "${PROJECT_SOURCE_DIR}/extern/lcio/include")
 
+  find_library(LCIO_LIBRARY NAMES lcio
+    HINTS "${LCIO}/lib" "$ENV{LCIO}/lib" "${PROJECT_SOURCE_DIR}/extern/lcio/release/lib")
+
+  find_library(LCIO_LIBRARY_DEBUG NAMES lcio
+    HINTS "${LCIO}/lib" "$ENV{LCIO}/lib" "${PROJECT_SOURCE_DIR}/extern/lcio/debug/lib")
+
+  set(LCIO_LIBRARIES_DEBUG ${LCIO_LIBRARY_DEBUG} )
+  set(LCIO_LIBRARIES ${LCIO_LIBRARY} )
+ELSE(WIN32)
   find_path(LCIO_INCLUDE_DIR lcio.h
     HINTS "${LCIO}/include" "$ENV{LCIO}/include")
 
   find_library(LCIO_LIBRARY NAMES lcio
     HINTS "${LCIO}/lib" "$ENV{LCIO}/lib")
+  
+  set(LCIO_LIBRARIES_DEBUG ${LCIO_LIBRARY} )
+  set(LCIO_LIBRARIES ${LCIO_LIBRARY} )
+ENDIF(WIN32)
 
-set(LCIO_LIBRARIES ${LCIO_LIBRARY} )
+
+
 set(LCIO_INCLUDE_DIRS ${LCIO_INCLUDE_DIR} )
 set(LCIO_DEFINITIONS "-DUSE_LCIO" )
 
