3993d10b49be50995328d2074cf3aa36d6368e73
Rpeschke <Richard.Peschke@desy.de>
2015-04-16 00:58:43 +0200

    formating

--- producers/exampleSCTProducer/src/SCTProducer.cxx
+++ producers/exampleSCTProducer/src/SCTProducer.cxx
@@ -16,122 +16,122 @@ static const std::string EVENT_TYPE = "Example";
 
 // Declare a new class that inherits from eudaq::Producer
 class SCTProducer : public eudaq::Producer {
-  public:
+public:
 
-    // The constructor must call the eudaq::Producer constructor with the name
-    // and the runcontrol connection string, and initialize any member variables.
-    SCTProducer(const std::string & name, const std::string & runcontrol)
-      : eudaq::Producer(name, runcontrol),
-      m_run(0), m_ev(0), stopping(false), done(false),started(0) {
-    
-      m_name = name;
+  // The constructor must call the eudaq::Producer constructor with the name
+  // and the runcontrol connection string, and initialize any member variables.
+  SCTProducer(const std::string & name, const std::string & runcontrol)
+    : eudaq::Producer(name, runcontrol),
+    m_run(0), m_ev(0), stopping(false), done(false), started(0) {
+
+    m_name = name;
+  }
+
+  // This gets called whenever the DAQ is configured
+  virtual void OnConfigure(const eudaq::Configuration & config) {
+    std::cout << "Configuring: " << config.Name() << std::endl;
+
+    // Do any configuration of the hardware here
+    // Configuration file values are accessible as config.Get(name, default)
+    m_exampleparam = config.Get("Parameter", 0);
+    std::cout << "Example Parameter = " << m_exampleparam << std::endl;
+    m_sct.openfile(config.Get("infile", "c:/slac/EventData_288_75.dat"));
+
+    // At the end, set the status that will be displayed in the Run Control.
+    SetStatus(eudaq::Status::LVL_OK, "Configured (" + config.Name() + ")");
+  }
+
+  // This gets called whenever a new run is started
+  // It receives the new run number as a parameter
+  virtual void OnStartRun(unsigned param) {
+    m_run = param;
+    m_ev = 0;
+
+    std::cout << "Start Run: " << m_run << std::endl;
+
+    // It must send a BORE to the Data Collector
+    eudaq::RawDataEvent bore(eudaq::RawDataEvent::BORE(m_name, m_run));
+    // You can set tags on the BORE that will be saved in the data file
+    // and can be used later to help decoding
+    bore.SetTag("EXAMPLE", eudaq::to_string(m_exampleparam));
+    // Send the event to the Data Collector
+    SendEvent(bore);
+
+    // At the end, set the status that will be displayed in the Run Control.
+    SetStatus(eudaq::Status::LVL_OK, "Running");
+    started = true;
+  }
+
+  // This gets called whenever a run is stopped
+  virtual void OnStopRun() {
+    std::cout << "Stopping Run" << std::endl;
+    started = false;
+    // Set a flag to signal to the polling loop that the run is over
+    stopping = true;
+
+    // wait until all events have been read out from the hardware
+    while (stopping) {
+      eudaq::mSleep(20);
     }
 
-    // This gets called whenever the DAQ is configured
-    virtual void OnConfigure(const eudaq::Configuration & config) {
-      std::cout << "Configuring: " << config.Name() << std::endl;
+    // Send an EORE after all the real events have been sent
+    // You can also set tags on it (as with the BORE) if necessary
+    SendEvent(eudaq::RawDataEvent::EORE(m_name, m_run, ++m_ev));
+  }
 
-      // Do any configuration of the hardware here
-      // Configuration file values are accessible as config.Get(name, default)
-      m_exampleparam = config.Get("Parameter", 0);
-      std::cout << "Example Parameter = " << m_exampleparam << std::endl;
-      m_sct.openfile(config.Get("infile", "c:/slac/EventData_288_75.dat"));
+  // This gets called when the Run Control is terminating,
+  // we should also exit.
+  virtual void OnTerminate() {
+    std::cout << "Terminating..." << std::endl;
+    done = true;
+  }
 
-      // At the end, set the status that will be displayed in the Run Control.
-      SetStatus(eudaq::Status::LVL_OK, "Configured (" + config.Name() + ")");
-    }
-
-    // This gets called whenever a new run is started
-    // It receives the new run number as a parameter
-    virtual void OnStartRun(unsigned param) {
-      m_run = param;
-      m_ev = 0;
-	  
-      std::cout << "Start Run: " << m_run << std::endl;
-
-      // It must send a BORE to the Data Collector
-      eudaq::RawDataEvent bore(eudaq::RawDataEvent::BORE(m_name, m_run));
-      // You can set tags on the BORE that will be saved in the data file
-      // and can be used later to help decoding
-      bore.SetTag("EXAMPLE", eudaq::to_string(m_exampleparam));
-      // Send the event to the Data Collector
-      SendEvent(bore);
-
-      // At the end, set the status that will be displayed in the Run Control.
-      SetStatus(eudaq::Status::LVL_OK, "Running");
-	  started=true;
-    }
-
-    // This gets called whenever a run is stopped
-    virtual void OnStopRun() {
-      std::cout << "Stopping Run" << std::endl;
-	  started=false;
-      // Set a flag to signal to the polling loop that the run is over
-      stopping = true;
-
-      // wait until all events have been read out from the hardware
-      while (stopping) {
-        eudaq::mSleep(20);
-      }
-
-      // Send an EORE after all the real events have been sent
-      // You can also set tags on it (as with the BORE) if necessary
-      SendEvent(eudaq::RawDataEvent::EORE(m_name, m_run, ++m_ev));
-    }
-
-    // This gets called when the Run Control is terminating,
-    // we should also exit.
-    virtual void OnTerminate() {
-      std::cout << "Terminating..." << std::endl;
-      done = true;
-    }
-
-    // This is just an example, adapt it to your hardware
-    void ReadoutLoop() {
-      // Loop until Run Control tells us to terminate
-      while (!done) {
-        if (!m_sct.hasData()) {
-          // No events are pending, so check if the run is stopping
-          if (stopping) {
-            // if so, signal that there are no events left
-            stopping = false;
-          }
-          // Now sleep for a bit, to prevent chewing up all the CPU
-          eudaq::mSleep(20);
-          // Then restart the loop
-          continue;
+  // This is just an example, adapt it to your hardware
+  void ReadoutLoop() {
+    // Loop until Run Control tells us to terminate
+    while (!done) {
+      if (!m_sct.hasData()) {
+        // No events are pending, so check if the run is stopping
+        if (stopping) {
+          // if so, signal that there are no events left
+          stopping = false;
         }
-		if (!started)
-		{
-			// Now sleep for a bit, to prevent chewing up all the CPU
-			eudaq::mSleep(20);
-			// Then restart the loop
-			continue;
-		}
-        // If we get here, there must be data to read out
-        // Create a RawDataEvent to contain the event data to be sent
-        eudaq::RawDataEvent ev(m_name, m_run, m_ev);
-
-        std::vector<unsigned char> buffer;
-        eudaq::bool2uchar(m_sct.data, m_sct.data + m_sct.size, buffer);
-          ev.AddBlock(0, buffer);
-        
-          m_sct.readLine(); 
-        // Send the event to the Data Collector      
-        SendEvent(ev);
-        // Now increment the event number
-        m_ev++;
+        // Now sleep for a bit, to prevent chewing up all the CPU
+        eudaq::mSleep(20);
+        // Then restart the loop
+        continue;
       }
-    }
+      if (!started)
+      {
+        // Now sleep for a bit, to prevent chewing up all the CPU
+        eudaq::mSleep(20);
+        // Then restart the loop
+        continue;
+      }
+      // If we get here, there must be data to read out
+      // Create a RawDataEvent to contain the event data to be sent
+      eudaq::RawDataEvent ev(m_name, m_run, m_ev);
 
-  private:
-    // This is just a dummy class representing the hardware
-    // It here basically that the example code will compile
-    // but it also generates example raw data to help illustrate the decoder
-    SCTDummy m_sct;
-    std::string m_name;
-    unsigned m_run, m_ev, m_exampleparam;
-    bool stopping, done,started;
+      std::vector<unsigned char> buffer;
+      eudaq::bool2uchar(m_sct.data, m_sct.data + m_sct.size, buffer);
+      ev.AddBlock(0, buffer);
+
+      m_sct.readLine();
+      // Send the event to the Data Collector      
+      SendEvent(ev);
+      // Now increment the event number
+      m_ev++;
+    }
+  }
+
+private:
+  // This is just a dummy class representing the hardware
+  // It here basically that the example code will compile
+  // but it also generates example raw data to help illustrate the decoder
+  SCTDummy m_sct;
+  std::string m_name;
+  unsigned m_run, m_ev, m_exampleparam;
+  bool stopping, done, started;
 };
 
 // The main function that will create a Producer instance and run it
@@ -139,14 +139,14 @@ int main(int /*argc*/, const char ** argv) {
   // You can use the OptionParser to get command-line arguments
   // then they will automatically be described in the help (-h) option
   eudaq::OptionParser op("EUDAQ Example Producer", "1.0",
-      "Just an example, modify it to suit your own needs");
+    "Just an example, modify it to suit your own needs");
   eudaq::Option<std::string> rctrl(op, "r", "runcontrol",
-      "tcp://localhost:44000", "address",
-      "The address of the RunControl.");
+    "tcp://localhost:44000", "address",
+    "The address of the RunControl.");
   eudaq::Option<std::string> level(op, "l", "log-level", "NONE", "level",
-      "The minimum level for displaying log messages locally");
-  eudaq::Option<std::string> name (op, "n", "name", "sct", "string",
-      "The name of this Producer");
+    "The minimum level for displaying log messages locally");
+  eudaq::Option<std::string> name(op, "n", "name", "sct", "string",
+    "The name of this Producer");
   try {
     // This will look through the command-line arguments and set the options
     op.Parse(argv);
@@ -158,7 +158,8 @@ int main(int /*argc*/, const char ** argv) {
     producer.ReadoutLoop();
     // When the readout loop terminates, it is time to go
     std::cout << "Quitting" << std::endl;
-  } catch (...) {
+  }
+  catch (...) {
     // This does some basic error handling of common exceptions
     return op.HandleMainException();
   }
