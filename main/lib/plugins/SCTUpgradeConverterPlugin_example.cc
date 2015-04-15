ef8e6b60a30ac7ab1946d73f41f8395d002136a6
Rpeschke <Richard.Peschke@desy.de>
2015-04-16 01:23:00 +0200

    added LCIO Converter

--- main/lib/plugins/SCTUpgradeConverterPlugin_example.cc
+++ main/lib/plugins/SCTUpgradeConverterPlugin_example.cc
@@ -53,6 +53,17 @@ namespace eudaq {
     // Again, this is just an example, adapted it for the actual data layout.
     virtual bool GetStandardSubEvent(StandardEvent & sev, const Event & ev) const;
 
+
+
+
+
+#if USE_LCIO
+    // This is where the conversion to LCIO is done
+    void GetLCIORunHeader(lcio::LCRunHeader & header, eudaq::Event const & /*bore*/, eudaq::Configuration const & /*conf*/) const;
+
+
+    bool GetLCIOSubEvent(lcio::LCEvent & result, const Event & source) const ;
+#endif
   private:
 
     // The constructor can be private, only one static instance is created
@@ -138,6 +149,100 @@ namespace eudaq {
     // Indicate that data was successfully converted
     return true;
   }
+#ifdef  USE_LCIO
 
 
+  bool Collection_createIfNotExist(lcio::LCCollectionVec** zsDataCollection, const lcio::LCEvent & lcioEvent, const char * name){
+
+    bool zsDataCollectionExists = false;
+    try
+    {
+      *zsDataCollection = static_cast<lcio::LCCollectionVec*> (lcioEvent.getCollection(name));
+      zsDataCollectionExists = true;
+    }
+    catch (lcio::DataNotAvailableException& e)
+    {
+      *zsDataCollection = new lcio::LCCollectionVec(lcio::LCIO::TRACKERDATA);
+    }
+
+    return zsDataCollectionExists;
+  }
+
+  void SCTupgradeConverterPlugin_sct1::GetLCIORunHeader(lcio::LCRunHeader & header, eudaq::Event const & /*bore*/, eudaq::Configuration const & /*conf*/) const {
+    eutelescope::EUTelRunHeaderImpl runHeader(&header);
+    runHeader.setDAQHWName(EUTELESCOPE::EUDRB); // should be:
+    //runHeader.setDAQHWName(EUTELESCOPE::NI);
+
+    // the information below was used by EUTelescope before the
+    // introduction of the BUI. Now all these parameters shouldn't be
+    // used anymore but they are left here for backward compatibility.
+
+    runHeader.setEUDRBMode("ZS");
+    runHeader.setEUDRBDet("SCT");
+    runHeader.setNoOfDetector(m_boards);
+    std::vector<int> xMin(m_boards, 0), xMax(m_boards, 1280), yMin(m_boards, 0), yMax(m_boards, 4);
+    runHeader.setMinX(xMin);
+    runHeader.setMaxX(xMax);
+    runHeader.setMinY(yMin);
+    runHeader.setMaxY(yMax);
+  }
+
+
+  bool SCTupgradeConverterPlugin_sct1::GetLCIOSubEvent(lcio::LCEvent & result, const Event & source) const {
+
+
+    if (source.IsBORE()) {
+      if (dbg > 0) std::cout << "SCTUpgradeConverterPlugin::GetLCIOSubEvent BORE " << std::endl;
+      // shouldn't happen
+      return true;
+    }
+    else if (source.IsEORE()) {
+      if (dbg > 0) std::cout << "SCTUpgradeConverterPlugin::GetLCIOSubEvent EORE " << std::endl;
+      // nothing to do
+      return true;
+    }
+    // If we get here it must be a data event
+
+    if (dbg > 0) std::cout << "SCTUpgradeConverterPlugin::GetLCIOSubEvent data " << std::endl;
+    result.parameters().setValue(eutelescope::EUTELESCOPE::EVENTTYPE, eutelescope::kDE);
+
+
+    LCCollectionVec *zsDataCollection = nullptr;
+    auto zsDataCollectionExists = Collection_createIfNotExist(&zsDataCollection, result, LCIO_collection_name);
+
+
+
+    StandardEvent tmp_evt;
+    GetStandardSubEvent(tmp_evt, source);
+    auto plane = tmp_evt.GetPlane(0);
+
+
+    // set the proper cell encoder
+    auto  zsDataEncoder = CellIDEncoder<TrackerDataImpl>(eutelescope::EUTELESCOPE::ZSDATADEFAULTENCODING, zsDataCollection);
+    zsDataEncoder["sensorID"] = plane.ID();
+    zsDataEncoder["sparsePixelType"] = eutelescope::kEUTelGenericSparsePixel;
+
+
+    // prepare a new TrackerData for the ZS data
+    auto zsFrame = std::unique_ptr<lcio::TrackerDataImpl>(new lcio::TrackerDataImpl());
+    zsDataEncoder.setCellID(zsFrame.get());
+
+
+    ConvertPlaneToLCIOGenericPixel(plane, *zsFrame);
+
+    // perfect! Now add the TrackerData to the collection
+    zsDataCollection->push_back(zsFrame.release());
+
+
+    if (!zsDataCollectionExists){
+      if (zsDataCollection->size() != 0)
+        result.addCollection(zsDataCollection, LCIO_collection_name);
+      else
+        delete zsDataCollection; // clean up if not storing the collection here
+    }
+
+    return true;
+  }
+
+#endif //  USE_LCIO
 } // namespace eudaq
