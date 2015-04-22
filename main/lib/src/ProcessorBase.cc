09ea7621aee503d57a9fa44cbe1e4eea349cda11
Richard Peschke <Richard.Peschke@desy.de>
2015-04-22 20:25:04 +0200

    base processor

---
+++ main/lib/src/ProcessorBase.cc
@@ -0,0 +1,49 @@
+#include "eudaq/ProcessorBase.hh"
+#include "eudaq/factoryDef.hh"
+#include "eudaq/OptionParser.hh"
+
+namespace eudaq{
+  
+  registerBaseClassDef(ProcessorBase);
+
+
+  ProcessorBase::ProcessorBase(Parameter_ref name) :m_conf(name)
+  {
+
+  }
+  std::unique_ptr<ProcessorBase> ProcessorFactory::create(ProcessorBase::MainType type, ProcessorBase::Parameter_ref param)
+  {
+
+    return EUDAQ_Utilities::Factory<ProcessorBase>::Create(type, param);
+  }
+
+  void ProcessorFactory::addComandLineOptions(eudaq::OptionParser & op)
+  {
+
+  }
+
+  std::string ProcessorFactory::Help_text()
+  {
+    return "help";
+  }
+
+  ProcessorBase::Parameter_t ProcessorFactory::getDefault()
+  {
+    return "default";
+  }
+
+  class ProcessorFactory::Impl{
+
+  public:
+    ProcessorBase::Parameter_t m_default_filePattern;
+
+  };
+
+  ProcessorFactory::Impl& ProcessorFactory::getImpl()
+  {
+    static ProcessorFactory::Impl m_impl;
+    return m_impl;
+  }
+
+  
+}
