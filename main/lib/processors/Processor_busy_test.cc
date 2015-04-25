8fb36e6a422b249e4d335be71d36738865af2ef4
Richard Peschke <Richard.Peschke@desy.de>
2015-04-25 18:59:55 +0200

    class to test if the busy system works

---
+++ main/lib/processors/Processor_busy_test.cc
@@ -0,0 +1,27 @@
+#include "eudaq/Processor_busy_test.hh"
+
+namespace eudaq{
+
+
+  using ReturnParam = ProcessorBase::ReturnParam;
+  RegisterProcessor(Processor_busy_test, "busy");
+  Processor_busy_test::Processor_busy_test(Parameter_ref conf) :Processor_Inspector(conf)
+  {
+
+  }
+
+  ReturnParam Processor_busy_test::inspecktEvent(const Event&)
+  {
+    ++m_counter;
+    if (m_counter<10)
+    {
+      return ProcessorBase::busy_retry;
+    }
+//     if (m_counter % 3 == 0){
+//       return ProcessorBase::busy_retry;
+//     }
+
+    return ProcessorBase::sucess;
+  }
+
+}
