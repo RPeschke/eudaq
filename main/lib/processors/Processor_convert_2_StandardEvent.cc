8e848d1d98b7b8e4db3675dbff392efcc4c68fc7
rpeschke <richard.peschke@desy.de>
2015-06-07 17:00:22 +0200

    new processor to convert events to standard events

---
+++ main/lib/processors/Processor_convert_2_StandardEvent.cc
@@ -0,0 +1,29 @@
+#include "eudaq/Processor.hh"
+#include "eudaq/PluginManager.hh"
+#include "eudaq/StandardEvent.hh"
+
+namespace eudaq{
+
+
+  class Processor_convert_2_StandardEvent :public Processor{
+    Processor_convert_2_StandardEvent(Parameter_t conf) :Processor(std::move(conf)){}
+
+    virtual ReturnParam ProcessEvent(event_sp ev);
+  };
+
+
+
+  using ReturnParam = ProcessorBase::ReturnParam;
+
+  ReturnParam Processor_convert_2_StandardEvent::ProcessEvent(event_sp ev)
+  {
+    auto devent = dynamic_cast<const DetectorEvent*>(ev.get());
+    if (ev->IsBORE())
+    {
+      eudaq::PluginManager::Initialize(*devent);
+    }
+    auto sev = event_sp(new StandardEvent(std::move(eudaq::PluginManager::ConvertToStandard(*devent))));
+    return ProcessNext(sev);
+  }
+  
+}
