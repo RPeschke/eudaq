3d892eb65a4462eb9599fa5fa0c0f8af91f3ee90
rpeschke <richard.peschke@desy.de>
2015-07-19 16:57:47 +0200

    file reader gets new interface to set the file name

--- main/include/eudaq/Processor_parallel_file_reader.hh
+++ main/include/eudaq/Processor_parallel_file_reader.hh
@@ -1,6 +1,7 @@
 #ifndef Processor_parallel_file_reader_h__
 #define Processor_parallel_file_reader_h__
 #include "eudaq/Processor_Parrallel_add2queue.hh"
+#include "eudaq/types/fileName.hh"
 
 
 namespace eudaq{
@@ -9,15 +10,20 @@ namespace eudaq{
 
   public:
     
-    Processor_parallel_file(Parameter_ref conf);
+    Processor_parallel_file(Parameter_ref conf,const fileName& fName);
+
     virtual ~Processor_parallel_file(){}
 	virtual void initialize() override;
   virtual void finish() override;
 
     virtual ReturnParam ProcessEvent(event_sp ev);
     virtual void print(std::ostream& os);
+    void setFileName(const std::string& filename);
   private:
     Processor_up m_reader = nullptr;
+    fileName m_fileName;
+  };
+
   };
 
 }
