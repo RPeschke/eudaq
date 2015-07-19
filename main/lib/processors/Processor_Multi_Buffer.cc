80da796730733aaaf783cf59e6485b9d013f2658
rpeschke <richard.peschke@desy.de>
2015-07-19 16:47:14 +0200

    processors are strongly typed therefore one has to use templates

--- main/lib/processors/Processor_Multi_Buffer.cc
+++ main/lib/processors/Processor_Multi_Buffer.cc
@@ -1,12 +1,13 @@
 
 #include "eudaq/Processor_parralel.hh"
+#include "eudaq/Processor_Buffer.hh"
 
 
 namespace eudaq{
 
-  class Processor_multi_buffer :public Processor_parallel{
+  class Processor_multi_buffer :public Processor_parallel<Proccessor_Buffer>{
   public:
-    Processor_multi_buffer(Parameter_ref conf) : Processor_parallel(conf +ProConfig::ProcessorParallelType(ProcessorNames::buffer())){
+    Processor_multi_buffer(Parameter_ref conf) : Processor_parallel<Proccessor_Buffer>(conf){
       
     }
 
