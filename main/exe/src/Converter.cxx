c1b76f188c2b57db3748172bcaf7a23a2efe4e76
rpeschke <richard.peschke@desy.de>
2015-10-18 15:45:20 +0200

    Converter is the first class to use the processor class

--- main/exe/src/Converter.cxx
+++ main/exe/src/Converter.cxx
@@ -11,8 +11,11 @@
 #include "eudaq/ProcessorBase.hh"
 #include "eudaq/RawDataEvent.hh"
 #include "eudaq/Platform.hh"
-
-
+#include "eudaq/Processor_batch.hh"
+#include "eudaq/ProcessorFileReader.hh"
+#include "eudaq/Processor_inspector.hh"
+#include "eudaq/ProcessorFileWriter.hh"
+#include "eudaq/Processor_eventSelectors.hh"
 using namespace eudaq;
 unsigned dbg = 0;
 
@@ -20,9 +23,8 @@ unsigned dbg = 0;
 
 int main(int, char ** argv) {
 
+  std::clock_t    mid_ = 0 ,end_=0;
 
-
-    std::clock_t    start = std::clock();
     eudaq::OptionParser op("EUDAQ File Converter", "1.0", "", 1);
 
     auto events = ReadAndProcess<eudaq::FileWriter>::add_Command_line_option_EventsOfInterest(op);
@@ -41,26 +43,35 @@ int main(int, char ** argv) {
     eudaq::Option<std::string> level(op, "l", "log-level", "INFO", "level",
                                      "The minimum level for displaying log messages locally");
 
+    std::clock_t    start = std::clock();
   try {
     op.Parse(argv);
     EUDAQ_LOG_LEVEL(level.Value());
 
-    ReadAndProcess<eudaq::FileWriter> readProcess;
-    readProcess.setEventsOfInterest(parsenumbers(events->Value()));
+    Processor_batch batch(ProcessorBase::Parameter_t(""));
 
-    readProcess.addFileReader(FileReaderFactory::create(op));
-    readProcess.setWriter(FileWriterFactory::Create());
-    
-    readProcess.StartRun();
-    readProcess.process();
-    readProcess.EndRun();
+    batch.pushNewProcessor<ProcessorFileReader>(op);
+    batch.pushProcessor(make_Processor([](const Event& ev, ProcessorBase::ConnectionName_ref con) {
+      if (ev.GetEventNumber()%1000==0)
+      {
+        std::cout << ev.GetEventNumber() << "  " <<con << std::endl;
+      }
+      return ProcessorBase::sucess;
+     }));
+    batch.pushNewProcessor<select_events>(parsenumbers(events->Value()));
+    batch.pushNewProcessor<ProcessorFileWriter>();
+
+    batch.init();
+    batch.run();
+    batch.end();
     
   }
   catch (...) {
     std::cout << "Time: " << (std::clock() - start) / (double) (CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
     return op.HandleMainException();
   }
-  std::cout << "Time: " << (std::clock() - start) / (double) (CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+  end_ = std::clock();
+  std::cout << "Time: " << (end_ - start) / (double) (CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
   if (dbg > 0)std::cout << "almost done with Converter. exiting" << std::endl;
   return 0;
 }
