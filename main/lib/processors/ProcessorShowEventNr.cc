d8971af7c933493c6d784510d573f6511fde01d2
rpeschke <richard.peschke@desy.de>
2015-10-18 18:52:38 +0200

    Updated the show event number processor to show the numbers a little bit nicer

--- main/lib/processors/ProcessorShowEventNr.cc
+++ main/lib/processors/ProcessorShowEventNr.cc
@@ -1,5 +1,7 @@
 #include "eudaq/Processor_inspector.hh"
 #include <tuple>
+#include "eudaq/Processor_showEventNr.hh"
+#include "eudaq/Processors.hh"
 
 
 using namespace std;
@@ -7,92 +9,65 @@ using namespace std;
 namespace eudaq{
   using ReturnParam = ProcessorBase::ReturnParam;
 
-  class ShowEventNR :public Processor_Inspector{
-  public:
-    virtual void initialize(Configuration_ref conf) {
-      firstEvent = true;
-    }
-    virtual void Finish() {
-    }
-    virtual ReturnParam inspecktEvent(const Event&);
+  ShowEventNR::ShowEventNR(size_t repetition) :m_rep(repetition) {
 
-    virtual void print(std::ostream& os);
-    class timeing{
-    public:
-      void reset(){
-        freq_all_time = 0;
-        freq_current = 0;
-        m_last_event = 0;
-        m_last_time = std::clock();
-        m_start_time = std::clock();
-      }
-
-      void processEvent(const Event& ev){
-        auto newClock = std::clock();
-
-        freq_all_time = ( (double)(newClock - m_start_time) / CLOCKS_PER_SEC) / (ev.GetEventNumber());
-
-        freq_current = ((double)(newClock - m_last_time) / CLOCKS_PER_SEC) / (ev.GetEventNumber() - m_last_event);
-        m_last_event = ev.GetEventNumber();
-        m_last_time = std::clock();
-      }
-      double freq_all_time = 0,
-        freq_current=0;
-    private:
-      unsigned m_last_event = 0;
-      clock_t m_last_time = 0,m_start_time=0;
-    } m_timming;
-    tuple<int, int> CalcRate(const Event& ev);
-    
-    ShowEventNR(Parameter_ref);
-    void show(const Event& ev);
-    size_t m_pos;
-    bool firstEvent = true;
-  };
-
-
-
-
-  void ShowEventNR::print(std::ostream& os)
-  {
-    os << getName();
   }
 
 
 
-  ShowEventNR::ShowEventNR(Parameter_ref conf) :Processor_Inspector(conf)
-  {
-    m_pos =4* conf.getPos();
+
+
+  void ShowEventNR::init() {
+    m_timming.reset();
   }
+
 
 
 
   void ShowEventNR::show(const Event& ev)
   {
     m_timming.processEvent(ev);
-    std::cout << std::string(m_pos, ' ') << getName() << ": " << ev.GetEventNumber() << " rate = " << m_timming.freq_current <<std::endl;
+    std::cout << "Event nr: " << ev.GetEventNumber() << " current rate = " << (int)m_timming.freq_current << " [events/s].  overall rate  = " << (int)m_timming.freq_all_time<< " [events/s]." <<std::endl;
   }
 
-  ReturnParam ShowEventNR::inspecktEvent(const Event& ev)
+  ReturnParam ShowEventNR::inspecktEvent(const Event& ev, ConnectionName_ref con)
   {
-    if (firstEvent )
-    {
-      m_timming.reset();
-    }
+
     if (ev.GetEventNumber() <10)
     {
       show(ev);
     }
-    else if (ev.GetEventNumber()% 100 ==0)
+    else if (ev.GetEventNumber() % m_rep == 0)
     {
       show(ev);
     }
    
     return sucess;
   }
+  void ShowEventNR::timeing::processEvent(const Event& ev) {
+    auto newClock = std::clock();
+
+    freq_all_time = (ev.GetEventNumber()) / ((double)(newClock - m_start_time) / CLOCKS_PER_SEC);
+
+    freq_current = (ev.GetEventNumber() - m_last_event) / ((double)(newClock - m_last_time) / CLOCKS_PER_SEC);
+    m_last_event = ev.GetEventNumber();
+    m_last_time = std::clock();
+  }
+  void ShowEventNR::timeing::reset() {
+    freq_all_time = 0;
+    freq_current = 0;
+    m_last_event = 0;
+    m_last_time = std::clock();
+    m_start_time = std::clock();
+  }
+
   Processors::processor_up Processors::ShowEventNR(size_t repetition) {
     return __make_unique<eudaq::ShowEventNR>(repetition);
   }
+
+
+
+
 
 }
 
