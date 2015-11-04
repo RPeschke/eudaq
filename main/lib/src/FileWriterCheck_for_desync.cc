aa5b134114e4b6f27a5836a7f039406df488fecf
rpeschke <richard.peschke@desy.de>
2015-11-04 19:45:22 +0100

    improved the algorithm to check for correlations still no sucess

--- main/lib/src/FileWriterCheck_for_desync.cc
+++ main/lib/src/FileWriterCheck_for_desync.cc
@@ -10,7 +10,7 @@ using namespace std;
 
 #define SIGMA_CUT_OFF 40
 #define peak_cutoff 5
-#define max_peak_value 1.5
+#define max_peak_value 3
 #define  size_of_my_array 10
 
 
@@ -29,7 +29,7 @@ struct mean_sigma {
 template <size_t NBins>
 class histoGram {
 public:
-  histoGram(double min_, double max_):m_min(min_),m_max(max_) {
+  histoGram(double min_, double max_) :m_min(min_), m_max(max_) {
     m_step = (m_max - m_min) / NBins;
 
     for (size_t i = 0; i < NBins; ++i) {
@@ -40,16 +40,15 @@ public:
 
   void fill(double x) {
     int index = (x - m_min) / m_step;
-    if (index>=NBins|| index<0)
-    {
+    if (index >= NBins || index < 0) {
       return;
     }
     m_bins[index]++;
   }
   std::vector<int> getBins_sorted() {
-    std::vector<int> ret(begin(m_bins),end(m_bins));
+    std::vector<int> ret(begin(m_bins), end(m_bins));
 
-    std::sort(begin(ret), end(ret),std::greater<int>());
+    std::sort(begin(ret), end(ret), std::greater<int>());
 
     return ret;
   }
@@ -73,39 +72,35 @@ template <size_t Nbins>
 class rollover_buffer {
 public:
   rollover_buffer() {
-    for (auto & e: m_buffer)
-    {
+    for (auto & e : m_buffer) {
       e = 0;
     }
   }
   void fill(double x) {
-    if (m_index>=Nbins)
-    {
+    if (m_index >= Nbins) {
       m_roll = true;
       m_index = 0;
     }
     m_buffer[m_index++] = x;
   }
-  
+
   double get_mean() const {
     m_roll = false;
     double sum_ = 0, counter_ = 0;
-    for (auto & e:m_buffer)
-    {
-      if (e>0)
-      {
+    for (auto & e : m_buffer) {
+      if (e > 0) {
         sum_ += e;
         ++counter_;
       }
     }
-    return sum_/counter_;
+    return sum_ / counter_;
   }
   bool has_Rollover()const {
     return m_roll;
   }
 private:
   mutable bool m_roll = false;
-  int m_index=0;
+  int m_index = 0;
   double m_buffer[Nbins];
 };
 namespace eudaq {
@@ -138,6 +133,9 @@ private:
   bool first_ = true;
   double m_avarage_peakness = 10000;
   histogram_t m_hist = histogram_t(-2000, 3000);
+  void handle_x_th_event(const DetectorEvent& devent);
+  void handle_x_th_event_cleanup();
+  int num_OF_BAD_runs = 0;
 };
 
 
@@ -155,7 +153,7 @@ size_t findPlaneById(const StandardEvent& sev, size_t id_) {
 
 fileWriterCheck_for_desync::fileWriterCheck_for_desync(const std::string & param)
   :firstEvent(false), m_out(nullptr) {
-  
+
   std::cout << "EUDAQ_DEBUG: This is fileWriterCheck_for_desync::fileWriterCheck_for_desync(" << param << ")" << std::endl;
 }
 
@@ -190,38 +188,8 @@ void fileWriterCheck_for_desync::WriteEvent(const DetectorEvent & devent) {
   }
   if (++m_counter > m_max_counter) {
 
-
-    auto hiracy = m_hist.getBins_sorted();
-    auto m = get_mean_sigma(m_res);
-    double sum_ = std::accumulate(begin(hiracy), end(hiracy), 0);
-    double peakness = m.sigma / ((double)hiracy[0]);
-    *m_out << devent.GetEventNumber() << " ; " << m.mean << "; " << m.sigma << " ; " << m_res.size() << " ; " <<peakness  << "\n";
-#ifdef _DEBUG
-    m_out->flush();
-#endif // _DEBUG
-    if (peakness > peak_cutoff * m_avarage_peakness|| peakness>max_peak_value) {
-      std::cout << "losing sync after " << devent.GetEventNumber() << " old sigma value " << m_avarage_peakness << "  new sigma value " << peakness << std::endl;
-      EUDAQ_THROW("losing sync");
-    }
-    if (fabs(peakness) < 1000) {
-      m_rollover.fill(peakness);
-    }
-    if (m_rollover.has_Rollover()) {
-      auto peak = m_rollover.get_mean();
-      if (peak>0)
-      {
-        m_avarage_peakness = peak;
-      }
-      else {
-        std::cout << " peak was zero " << std::endl;
-      }
-    }
-    
-    m_counter = 0;
-    m_res.clear();
-    m_hist = histogram_t(-2000, 3000);
-    m_old_mean_sigma = m;
-    first_ = false;
+    handle_x_th_event(devent);
+    handle_x_th_event_cleanup();
   }
   StandardEvent sev = eudaq::PluginManager::ConvertToStandard(devent);
 
@@ -270,7 +238,7 @@ void fileWriterCheck_for_desync::run_plane(const planehit& planeA_hit, const Sta
 void fileWriterCheck_for_desync::push_to_vector(const planehit& planeA_hit, const planehit& planeB_hit) {
 
 
-  auto res = planeA_hit.x - 3.19042*planeB_hit.x ;
+  auto res = planeA_hit.x - 3.19042*planeB_hit.x;
   if (!first_) {
     if (res > m_old_mean_sigma.mean + SIGMA_CUT_OFF
         ||
@@ -281,6 +249,58 @@ void fileWriterCheck_for_desync::push_to_vector(const planehit& planeA_hit, cons
   }
   m_hist.fill(res);
   m_res.push_back(res);
+}
+
+void fileWriterCheck_for_desync::handle_x_th_event(const DetectorEvent& devent) {
+
+
+  auto hiracy = m_hist.getBins_sorted();
+  auto m = get_mean_sigma(m_res);
+  double sum_ = std::accumulate(begin(hiracy), end(hiracy), 0);
+
+
+  if (sum_ == 0) {
+    return;
+  }
+  double peakness = m.sigma / ((double)hiracy[0]);
+  *m_out << devent.GetEventNumber() << " ; " << m.mean << "; " << m.sigma << " ; " << m_res.size() << " ; " << hiracy[0] << "; " << peakness << "\n";
+#ifdef _DEBUG
+  m_out->flush();
+#endif // _DEBUG
+  if (peakness > peak_cutoff * m_avarage_peakness || peakness > max_peak_value) {
+
+    std::cout << "losing sync after " << devent.GetEventNumber() << " old sigma value " << m_avarage_peakness << "  new sigma value " << peakness << std::endl;
+    if (++num_OF_BAD_runs > 3) {
+      EUDAQ_THROW("losing sync");
+    }
+    return;
+  }
+
+  if (fabs(peakness) > 1000) {
+    return;
+  }
+  m_rollover.fill(peakness);
+  if (m_rollover.has_Rollover()) {
+    auto peak = m_rollover.get_mean();
+    if (peak > 0) {
+      m_avarage_peakness = peak;
+    } else {
+      std::cout << " peak was zero " << std::endl;
+    }
+  }
+  if (num_OF_BAD_runs>0)
+  {
+  num_OF_BAD_runs--;
+  }
+  m_old_mean_sigma = m;
+}
+
+void fileWriterCheck_for_desync::handle_x_th_event_cleanup() {
+  m_counter = 0;
+  m_res.clear();
+  m_hist = histogram_t(-2000, 3000);
+
+  first_ = false;
 }
 
 uint64_t fileWriterCheck_for_desync::FileBytes() const { return m_out->tellp(); }
