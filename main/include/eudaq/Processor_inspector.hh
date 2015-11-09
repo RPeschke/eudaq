8a31bbc0bf4579704e4b20c956fa19ceb92831bf
rpeschke <richard.peschke@desy.de>
2015-11-09 13:18:09 +0100

    added new function to  add lambda function to the processor batch

--- main/include/eudaq/Processor_inspector.hh
+++ main/include/eudaq/Processor_inspector.hh
@@ -8,9 +8,37 @@
 
 
 
+#define ADD_LAMBDA_PROZESSOR() hidden::___processor_dummy_void() + []()
+#define ADD_LAMBDA_PROZESSOR(event_ref) hidden::___processor_dummy_event() + [](const eudaq::Event& event_ref) 
+#define ADD_LAMBDA_PROZESSOR(event_ref,COnnectionName) hidden::___processor_dummy_full() + [](const eudaq::Event& event_ref, size_t COnnectionName)
+
 namespace eudaq {
 
+namespace hidden {
+class ___processor_dummy_void {
 
+};
+template <typename T>
+Processor_up operator+(const ___processor_dummy_void &, T&& t) {
+  return make_Processor_void(std::forward<T>(t));
+}
+
+class ___processor_dummy_event {
+
+};
+template <typename T>
+Processor_up operator+(const ___processor_dummy_event &, T&& t) {
+  return make_Processor_event(std::forward<T>(t));
+}
+
+class ___processor_dummy_full{
+
+};
+template <typename T>
+Processor_up operator+(const ___processor_dummy_full &, T&& t) {
+  return make_Processor(std::forward<T>(t));
+}
+}
 
   class DLLEXPORT Processor_Inspector :public ProcessorBase {
   public:
@@ -37,25 +65,76 @@ namespace eudaq {
     
 
     virtual ReturnParam inspecktEvent(const Event& e, ConnectionName_ref con) override {
-      
-      return m_proc(e,con);
+      m_proc(e, con);
+      return ProcessorBase::sucess;
+    }
+  private:
+    T m_proc;
+
+  };
+
+  template <typename T>
+  class  processor_T_void : public Processor_Inspector {
+  public:
+    processor_T_void(Parameter_ref conf, T&& proc_) :Processor_Inspector(conf), m_proc(std::forward<T>(proc_)) {}
+
+    processor_T_void(T&& proc_) : m_proc(std::forward<T>(proc_)) {}
+
+
+    virtual ReturnParam inspecktEvent(const Event& e, ConnectionName_ref con) override {
+      m_proc();
+      return ProcessorBase::sucess;
     }
   private:
     T m_proc;
 
   };
   template <typename T>
-  processor_T<T> make_p(T&& proc_) {
-    return processor_T<T>(std::forward<T>(proc_));
-  }
-  template <typename T>
-  std::unique_ptr<processor_T<T>> make_Processor(eudaq::ProcessorBase::Parameter_ref conf, T&& proc_) {
-    return std::unique_ptr<processor_T<T>>(new processor_T<T>(conf, std::forward<T>(proc_)));
-  }
-  template <typename T>
-  std::unique_ptr<processor_T<T>> make_Processor(T&& proc_) {
+  class  processor_T_event: public Processor_Inspector {
+  public:
+    processor_T_event(Parameter_ref conf, T&& proc_) :Processor_Inspector(conf), m_proc(std::forward<T>(proc_)) {}
 
-    return  std::unique_ptr<processor_T<T>>( new processor_T<T>(  std::forward<T>(proc_)));
+    processor_T_event(T&& proc_) : m_proc(std::forward<T>(proc_)) {}
+
+
+    virtual ReturnParam inspecktEvent(const Event& e, ConnectionName_ref con) override {
+      m_proc(e);
+      return  ProcessorBase::sucess;
+    }
+  private:
+    T m_proc;
+
+  };
+
+  template <typename T>
+  Processor_up make_Processor(eudaq::ProcessorBase::Parameter_ref conf, T&& proc_) {
+    return Processor_up(new processor_T<T>(conf, std::forward<T>(proc_)));
+  }
+  template <typename T>
+  Processor_up make_Processor(T&& proc_) {
+
+    return  Processor_up(new processor_T<T>(std::forward<T>(proc_)));
+  }
+
+  template <typename T>
+  Processor_up make_Processor_void(eudaq::ProcessorBase::Parameter_ref conf, T&& proc_) {
+    return Processor_up(new processor_T_void<T>(conf, std::forward<T>(proc_)));
+  }
+  template <typename T>
+  Processor_up make_Processor_void(T&& proc_) {
+
+    return  Processor_up(new processor_T_void<T>(std::forward<T>(proc_)));
+  }
+  
+  
+  template <typename T>
+  Processor_up make_Processor_event(eudaq::ProcessorBase::Parameter_ref conf, T&& proc_) {
+    return Processor_up(new processor_T_void<T>(conf, std::forward<T>(proc_)));
+  }
+  template <typename T>
+  Processor_up make_Processor_event(T&& proc_) {
+
+    return  Processor_up(new processor_T_event<T>(std::forward<T>(proc_)));
   }
 }
 #endif // Processor_inspector_h__
