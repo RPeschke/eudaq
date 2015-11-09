1c94d2ea56220f5fe0570865842189c4f5c7738d
rpeschke <richard.peschke@desy.de>
2015-11-09 19:04:29 +0100

    implemented code for get Configuration executable

--- main/exe/src/getConfiguration.cxx
+++ main/exe/src/getConfiguration.cxx
@@ -1,10 +1,9 @@
-#include "eudaq/ProcessorBase.hh"
 #include "eudaq/OptionParser.hh"
-#include "eudaq/FileWriter.hh"
 #include "eudaq/Logger.hh"
 #include "eudaq/Processor_inspector.hh"
+#include "eudaq/Processor_batch.hh"
+#include "eudaq/Processors.hh"
 #include "eudaq/baseFileReader.hh"
-#include "eudaq/EventSynchronisationBase.hh"
 
 using namespace eudaq;
 
@@ -14,41 +13,45 @@ using namespace eudaq;
 
 
 int main(int /*argc*/, const char ** argv) {
- 
+
 
   std::clock_t    start = std::clock();
-  eudaq::OptionParser op("EUDAQ File Converter", "1.0", "", 1);
+  eudaq::OptionParser op("EUDAQ Configuration Extractor", "1.0", "", 1);
 
 
   FileReaderFactory::addComandLineOptions(op);
 
   op.ExtraHelpText(FileReaderFactory::Help_text());
 
-  EventSyncFactory::addComandLineOptions(op);
+
 
   eudaq::Option<std::string> level(op, "l", "log-level", "INFO", "level",
-    "The minimum level for displaying log messages locally");
+                                   "The minimum level for displaying log messages locally");
 
   try {
-  op.Parse(argv);
-  EUDAQ_LOG_LEVEL(level.Value());
+    op.Parse(argv);
+    EUDAQ_LOG_LEVEL(level.Value());
 
 
-// 
-//   auto pro = ProcessorFactory::create(ProcessorNames::batch(), "");
-//   pro->pushProducer(ProcessorFactory::create(ProcessorNames::file_reader(), ProConfig::ProcessorName("first")));
-//   
-//   pro->pushProducer(ProcessorFactory::create(ProcessorNames::Print_Configuration(), ProConfig::ProcessorName("config")));
-//   pro->pushProducer(ProcessorFactory::create(ProcessorNames::SelectEvents(), ProConfig::ProcessorName("eventOfintresst")));
-//   pro->pushProducer(ProcessorFactory::create(ProcessorNames::show_event_nr(), "event_nr"));
+    Processor_batch batch;
+    for (size_t i = 0; i < op.NumArgs(); ++i) {
+      batch >> Processors::fileReader(fileConfig(op.GetArg(i)));
 
+    }
 
-//  auto conf = ProConfig::Topic("first") + ProConfig::Filename(op.GetArg(0)) + ProConfig::getConfig_configuration("config", ProConfig::getConfig_ConcatSectionsTag("", "")) +ProConfig::eventSelection_configuration("eventOfintresst","1,2,3",1,1);
-//   pro->init(conf);
-//   pro->ProcessEvent(nullptr);
-//   pro->end();
-  }
-  catch (...) {
+    batch >> Processors::eventSelector({ 0, 1 }, true);
+    batch >> ADD_LAMBDA_PROZESSOR1(ev) {
+      if (ev.IsBORE()) {
+        eudaq::Configuration conf(ev.GetTag("CONFIG"));
+        conf.Save(std::cout);
+      }
+    };
+
+    batch.init();
+    batch.run();
+    batch.end();
+
+  } catch (...) {
     std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
     return op.HandleMainException();
   }
