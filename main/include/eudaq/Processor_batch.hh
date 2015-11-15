b2655145c04e991b82d90dc204f4969977b7063a
rpeschke <richard.peschke@desy.de>
2015-11-15 06:02:34 +0100

    the batch can now also handle raw pointer.

--- main/include/eudaq/Processor_batch.hh
+++ main/include/eudaq/Processor_batch.hh
@@ -7,34 +7,42 @@
 #include <memory>
 #include "Processor_inspector.hh"
 namespace eudaq {
-
+using processor_i_up = std::unique_ptr<Processor_Inspector>;
 class DLLEXPORT Processor_batch :public ProcessorBase {
 
 public:
   virtual ReturnParam ProcessEvent(event_sp ev, ConnectionName_ref con) override;
   Processor_batch(Parameter_ref name);
   Processor_batch();
-  virtual ~Processor_batch() {}
+  virtual ~Processor_batch();
   void init() override;
   void end() override;
   void pushProcessor(Processor_up processor);
+  void pushProcessor(Processor_rp processor);
   void wait() override;
   void run();
+  void reset();
   template <class T, class... Args>
   void pushNewProcessor(Args&&... args) {
     pushProcessor(make_Processor_up<T>(args...));
   }
 private:
   std::unique_ptr<std::vector<Processor_up>> m_processors;
-
+  std::vector<Processor_rp> m_processors_rp;
   Processor_rp m_last = nullptr ,m_first =nullptr;
 };
 DLLEXPORT Processor_batch& operator>> (Processor_batch& batch, Processor_up proc);
+DLLEXPORT Processor_batch& operator>> (Processor_batch& batch, processor_i_up proc);
+DLLEXPORT Processor_batch& operator>> (Processor_batch& batch, Processor_rp proc);
 
-
+DLLEXPORT std::unique_ptr<Processor_batch> make_batch();
 template <typename T>
 Processor_batch& operator>> (Processor_batch& batch, T&& lamdbaProcessor) {
   return batch >> make_Processor(std::forward<T>(lamdbaProcessor));
+}
+template <typename T>
+Processor_batch& operator>> (Processor_batch& batch, T* Processor) {
+  return batch >> dynamic_cast<Processor_rp> (Processor);
 }
 
 }
--- main/lib/processors/Processor_batch.cc
+++ main/lib/processors/Processor_batch.cc
@@ -45,21 +45,25 @@ Processor_batch::Processor_batch():Processor_batch(Parameter_t("")) {
 
 }
 
+Processor_batch::~Processor_batch() {
+
+}
+
 ReturnParam Processor_batch::ProcessEvent(event_sp ev, ConnectionName_ref con) {
   return m_first->ProcessEvent(ev, con);
 }
 
 void Processor_batch::init() {
   
-  for (auto& e : reverse(*m_processors))
+  for (auto& e : reverse(m_processors_rp))
   {
     e->init();
   }
-  m_first = m_processors->front().get();
+  m_first = m_processors_rp.front();
 }
 
 void Processor_batch::end() {
-  for (auto& e : *m_processors) {
+  for (auto& e : m_processors_rp) {
     e->end();
   }
 }
@@ -67,23 +71,33 @@ void Processor_batch::end() {
 void Processor_batch::pushProcessor(Processor_up processor) {
   if (!processor)
   {
-    return;
+    EUDAQ_THROW("trying to push Empty Processor to batch");
   }
+  pushProcessor(processor.get());
   m_processors->push_back(std::move(processor));
-  auto new_last = m_processors->back().get();
-  if (m_last) {
-    m_last->AddNextProcessor(new_last);
-  }
 
-  m_last = new_last;
+
 
 }
 
 
 
+void Processor_batch::pushProcessor(Processor_rp processor) {
+  if (!processor) {
+    EUDAQ_THROW("trying to push Empty Processor to batch");
+  }
+
+  m_processors_rp.push_back(processor);
+  auto new_last = m_processors_rp.back();
+  if (m_last) {
+    m_last->AddNextProcessor(new_last);
+  }
+
+  m_last = new_last;
+}
+
 void Processor_batch::wait() {
-  for (auto& e:*m_processors)
-  {
+  for (auto& e : m_processors_rp) {
     e->wait();
   }
 } 
@@ -99,9 +113,31 @@ void Processor_batch::run() {
   wait();
 }
 
+void Processor_batch::reset() {
+  for (auto& e : m_processors_rp) {
+    e->wait();
+  }
+  m_processors->clear();
+  m_processors_rp.clear();
+}
+
 Processor_batch& operator>>(Processor_batch& batch, Processor_up proc) {
   batch.pushProcessor(std::move(proc));
   return batch;
 }
+
+ Processor_batch& operator>>(Processor_batch& batch, Processor_rp proc) {
+   batch.pushProcessor(proc);
+   return batch;
+}
+
+ Processor_batch& operator>>(Processor_batch& batch, processor_i_up proc) {
+   batch.pushProcessor(std::move(proc));
+   return batch;
+ }
+
+ std::unique_ptr<Processor_batch> make_batch() {
+   return std::unique_ptr<Processor_batch>(new Processor_batch());
+ }
 
 }
